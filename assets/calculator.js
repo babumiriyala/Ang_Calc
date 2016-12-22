d4e5e1ff165f9b8d1dc45d58827a074ac40c743c
Babu Miryala <babu.miryala@gmail.com>
2016-12-22 19:32:25 +0530

    updated Pricetag

--- assets/calculator.js
+++ assets/calculator.js
@@ -6,9 +6,25 @@
     app.controller('CalculatorController', ['$scope', function($scope){
 
 
-        alert('Sucess');
+        $scope.lumen_options = [300, 600, 900, 1125, 1600];
+        $scope.current_lumens = 600;
+        $scope.current_cost = 13;
+        $scope.current_hours = 4;
+        $scope.total_days = 365;
 
+        $scope.inc_conversion = .0625;
+        $scope.hal_conversion = .0450;
+        $scope.cfl_conversion = .0146;
+        $scope.led_conversion = .0125;
 
+        $scope.calculate = function () {
+           $scope.inc_wattage = ($scope.current_lumens * $scope.inc_conversion).toFixed(1);
+           $scope.hal_wattage = ($scope.current_lumens * $scope.hal_conversion).toFixed(1);
+           $scope.cfl_wattage = ($scope.current_lumens * $scope.cfl_conversion).toFixed(1);
+           $scope.led_wattage = ($scope.current_lumens * $scope.led_conversion).toFixed(1);
+        }
+
+        $scope.calculate();
     }]);
 
 })();
